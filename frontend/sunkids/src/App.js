ad310a28d996fd352014963a96683b929b92f8a4
HozifaAbbara <mhd.hozifa.abbara@gmail.com>
2025-02-25 22:46:37 +0300

    initialize frontend

--- frontend/sunkids/src/App.js
+++ frontend/sunkids/src/App.js
@@ -1,12 +1,7 @@
+import React from "react";
 
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
-}
+const App = () => {
+    return (<h1>Hell Yeah!</h1>);
+};
 
 export default App;
